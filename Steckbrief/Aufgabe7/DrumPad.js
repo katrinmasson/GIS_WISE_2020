91d6ba3efeafe03d81c8ff07b7b2c7ee1d2349e4
katrinmasson <72865322+katrinmasson@users.noreply.github.com>
2020-12-06 16:21:47 +0100

    Aufgabe 7

---
+++ Steckbrief/Aufgabe7/DrumPad.js
@@ -0,0 +1,55 @@
+/* Audios */
+var sample = [];
+sample[0] = new Audio("Audios/audio1.mp3");
+sample[1] = new Audio("Audios/audio2.mp3");
+sample[2] = new Audio("Audios/audio3.mp3");
+sample[3] = new Audio("Audios/audio4.mp3");
+sample[4] = new Audio("Audios/audio5.mp3");
+sample[5] = new Audio("Audios/audio6.mp3");
+sample[6] = new Audio("Audios/audio7.mp3");
+sample[7] = new Audio("Audios/audio8.mp3");
+sample[8] = new Audio("Audios/audio9.mp3");
+/* Audios abspielen lassen (Funktion allgemein und Playbutton) */
+function playSample(Sound) {
+    sample[Sound].play();
+}
+function playLoop(hihat, kick, snare) {
+    setInterval(function () {
+        sample[hihat].play();
+        sample[kick].play();
+        sample[snare].play();
+    }, 500);
+}
+;
+/* einzelne Drumpads */
+document.querySelector(".Pad1").addEventListener("click", function () {
+    playSample(0);
+});
+document.querySelector(".Pad2").addEventListener("click", function () {
+    playSample(1);
+});
+document.querySelector(".Pad3").addEventListener("click", function () {
+    playSample(2);
+});
+document.querySelector(".Pad4").addEventListener("click", function () {
+    playSample(3);
+});
+document.querySelector(".Pad5").addEventListener("click", function () {
+    playSample(4);
+});
+document.querySelector(".Pad6").addEventListener("click", function () {
+    playSample(5);
+});
+document.querySelector(".Pad7").addEventListener("click", function () {
+    playSample(6);
+});
+document.querySelector(".Pad8").addEventListener("click", function () {
+    playSample(7);
+});
+document.querySelector(".Pad9").addEventListener("click", function () {
+    playSample(8);
+});
+document.querySelector(".play").addEventListener('click', function () {
+    playLoop(4, 5, 8);
+});
+//# sourceMappingURL=DrumPad.js.map
