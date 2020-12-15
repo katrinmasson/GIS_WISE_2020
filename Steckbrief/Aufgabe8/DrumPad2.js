d19337e7e6ece6c2fcc871c9fd010d2e882f08da
katrinmasson <72865322+katrinmasson@users.noreply.github.com>
2020-12-15 13:38:30 +0100

    VerbesserungAufgabe8

--- Steckbrief/Aufgabe8/DrumPad2.js
+++ Steckbrief/Aufgabe8/DrumPad2.js
@@ -1,108 +1,157 @@
 // Audios 
-var sample = [];
-sample[0] = new Audio("./Audios/Audio1.mp3");
-sample[1] = new Audio("./Audios/Audio2.mp3");
-sample[2] = new Audio("./Audios/Audio3.mp3");
-sample[3] = new Audio("./Audios/Audio4.mp3");
-sample[4] = new Audio("./Audios/Audio5.mp3");
-sample[5] = new Audio("./Audios/Audio6.mp3");
-sample[6] = new Audio("./Audios/Audio7.mp3");
-sample[7] = new Audio("./Audios/Audio8.mp3");
-sample[8] = new Audio("./Audios/Audio9.mp3");
-// einzelne Drumpads 
-document.getElementById("pad1").addEventListener("click", function () {
-    playSample(0);
-    recordBeat(0);
-});
-document.getElementById("pad2").addEventListener("click", function () {
-    playSample(1);
-    recordBeat(1);
-});
-document.getElementById("pad3").addEventListener("click", function () {
-    playSample(2);
-    recordBeat(2);
-});
-document.getElementById("pad4").addEventListener("click", function () {
-    playSample(3);
-    recordBeat(3);
-});
-document.getElementById("pad5").addEventListener("click", function () {
-    playSample(4);
-    recordBeat(4);
-});
-document.getElementById("pad6").addEventListener("click", function () {
-    playSample(5);
-    recordBeat(5);
-});
-document.getElementById("pad7").addEventListener("click", function () {
-    playSample(6);
-    recordBeat(6);
-});
-document.getElementById("pad8").addEventListener("click", function () {
-    playSample(7);
-    recordBeat(7);
-});
-document.getElementById("pad9").addEventListener("click", function () {
-    playSample(8);
-    recordBeat(8);
-});
-// Funktion zum Abspielen lassen 
-function playSample(i) {
-    sample[i].play();
-    sample[i].recordBeat[];
+var pad1 = new Audio("./Audios/Audio1.mp3");
+var pad2 = new Audio("./Audios/Audio2.mp3");
+var pad3 = new Audio("./Audios/Audio3.mp3");
+var pad4 = new Audio("./Audios/Audio4.mp3");
+var pad5 = new Audio("./Audios/Audio5.mp3");
+var pad6 = new Audio("./Audios/Audio6.mp3");
+var pad7 = new Audio("./Audios/Audio7.mp3");
+var pad8 = new Audio("./Audios/Audio8.mp3");
+var pad9 = new Audio("./Audios/Audio9.mp3");
+// Funktion
+function playSample(pad) {
+    pad.play();
+    recordBeat(pad);
 }
-;
-//Play- und Pause-Button
+document.getElementById("pad1").addEventListener("click", playSample.bind(null, pad1));
+document.getElementById("pad2").addEventListener("click", playSample.bind(null, pad2));
+document.getElementById("pad3").addEventListener("click", playSample.bind(null, pad3));
+document.getElementById("pad4").addEventListener("click", playSample.bind(null, pad4));
+document.getElementById("pad5").addEventListener("click", playSample.bind(null, pad5));
+document.getElementById("pad6").addEventListener("click", playSample.bind(null, pad6));
+document.getElementById("pad7").addEventListener("click", playSample.bind(null, pad7));
+document.getElementById("pad8").addEventListener("click", playSample.bind(null, pad8));
+document.getElementById("pad9").addEventListener("click", playSample.bind(null, pad9));
+//Buttons: Mülleimer, Play, Pause, Micro, info
 var playButton = document.getElementById("play");
 var pauseButton = document.getElementById("pause");
-var array = [];
-var padAufnahme;
-//Play- und Pause-Button verschwinden lassen
+var microButton = document.getElementById("micro");
+var muellButton = document.getElementById("muell");
+var infoButton = document.getElementById("info");
+var infoText = document.getElementById("bild");
+// Der Anfangs-Beat
+var anfangsBeat = [];
+//Überprüfung von false/true
+var is;
+//Löschfunktion für den Mülleimer 
+function deletBeat() {
+    anfangsBeat.splice(0, anfangsBeat.length);
+    console.log(anfangsBeat.length);
+}
+//Veränderung des Micro-Buttons wenn er "aktiv" ist
+window.addEventListener("load", function () {
+    microButton.addEventListener("click", function () {
+        if (microButton.classList.contains("inactiv")) {
+            this.classList.remove("inactiv");
+            this.classList.add("activ");
+            is = true;
+        }
+        else {
+            microButton.classList.contains("activ");
+            this.classList.remove("activ");
+            this.classList.add("inactiv");
+            is = false;
+        }
+        console.log(is);
+        console.log(anfangsBeat.length);
+    });
+});
+// Löschen beim "click" auf den Mülleimer-Button
+muellButton.addEventListener("click", function () {
+    deletBeat();
+    console.log(anfangsBeat.length);
+});
+//Aufnahmefunktion
+function recordBeat(pad) {
+    console.log(is);
+    if (is == true) {
+        anfangsBeat.push(pad);
+        console.log(anfangsBeat.length);
+    }
+}
+;
+//Wiedergabe/Play- und Paus-Button
+var interval;
+var beat = [];
+var index = 0;
+function playLoop() {
+    interval = setInterval(function () {
+        if (index < beat.length) {
+            playSample(beat[index]);
+            index++;
+        }
+        else {
+            index = 0;
+        }
+    }, 300);
+}
+;
+playButton.addEventListener("click", function () {
+    interval = setInterval(function () {
+        if (index < anfangsBeat.length) {
+            playSample(anfangsBeat[index]);
+            index++;
+        }
+        else {
+            index = 0;
+        }
+    }, 500);
+});
 playButton.addEventListener("click", function () {
     this.classList.add("is-hidden");
     pauseButton.classList.remove("is-hidden");
-    padAufnahme = true;
 });
 pauseButton.addEventListener("click", function () {
     this.classList.add("is-hidden");
     playButton.classList.remove("is-hidden");
 });
-//Microfon-Button + Mülleimer-Button
-var microButton = document.getElementById("micro");
-var muellButton = document.getElementById("muell");
-microButton.addEventListener("click", function () {
-    if (microButton.classList.contains("activ")) {
-        this.classList.remove("activ");
-        padAufnahme = false;
-    }
-    else {
-        microButton.classList.add("activ");
-        padAufnahme = true;
-    }
+pauseButton.addEventListener("click", function () {
+    clearInterval(interval);
 });
-//Funktion zum aufnehmen
-function recordBeat(index) {
-    if (padAufnahme == true) {
-        array.push(index);
-    }
-}
-//Play und Pause
-var interval;
-var o = 0;
-playButton.addEventListener("click", function () {
-    if (padAufnahme == true) {
-        interval = setInterval(function () {
-            if ((o < array.length)) {
-                playSample(array[o]);
-                o++;
-            }
-            else {
-                o = 0;
-            }
-        }, 300);
-    }
-    else {
-        clearInterval(interval);
+//Info-Button
+infoButton.addEventListener("click", function () {
+    this.classList.add("activ");
+    infoText.classList.remove("is-hidden");
+});
+//Bedienen der Pads mit der Tastatur
+document.addEventListener("keydown", function (event) {
+    switch (event.key) {
+        case "z":
+            playSample(pad1);
+            recordBeat(pad1);
+            break;
+        case "u":
+            playSample(pad2);
+            recordBeat(pad2);
+            break;
+        case "i":
+            playSample(pad3);
+            recordBeat(pad3);
+            break;
+        case "h":
+            playSample(pad4);
+            recordBeat(pad4);
+            break;
+        case "j":
+            playSample(pad5);
+            recordBeat(pad5);
+            break;
+        case "k":
+            playSample(pad6);
+            recordBeat(pad6);
+            break;
+        case "b":
+            playSample(pad7);
+            recordBeat(pad7);
+            break;
+        case "n":
+            playSample(pad8);
+            recordBeat(pad8);
+            break;
+        case "m":
+            playSample(pad9);
+            recordBeat(pad9);
+            break;
     }
 });
 //# sourceMappingURL=DrumPad2.js.map
--- Steckbrief/Aufgabe8/DrumPad2.ts
+++ Steckbrief/Aufgabe8/DrumPad2.ts
@@ -1,132 +1,181 @@
     // Audios 
 
-    const sample: HTMLAudioElement[] = [];
-    sample[0] = new Audio("./Audios/Audio1.mp3");
-    sample[1] = new Audio("./Audios/Audio2.mp3");
-    sample[2] = new Audio("./Audios/Audio3.mp3");
-    sample[3] = new Audio("./Audios/Audio4.mp3");
-    sample[4] = new Audio("./Audios/Audio5.mp3");
-    sample[5] = new Audio("./Audios/Audio6.mp3");
-    sample[6] = new Audio("./Audios/Audio7.mp3");
-    sample[7] = new Audio("./Audios/Audio8.mp3");
-    sample[8] = new Audio("./Audios/Audio9.mp3");
+    var pad1 : HTMLAudioElement= new Audio ("./Audios/Audio1.mp3");
+    var pad2 : HTMLAudioElement = new Audio ("./Audios/Audio2.mp3");
+    var pad3 : HTMLAudioElement = new Audio ("./Audios/Audio3.mp3");
+    var pad4 : HTMLAudioElement = new Audio ("./Audios/Audio4.mp3");
+    var pad5 : HTMLAudioElement = new Audio ("./Audios/Audio5.mp3");
+    var pad6 : HTMLAudioElement = new Audio ("./Audios/Audio6.mp3");
+    var pad7 : HTMLAudioElement = new Audio ("./Audios/Audio7.mp3");
+    var pad8 : HTMLAudioElement = new Audio ("./Audios/Audio8.mp3");
+    var pad9 : HTMLAudioElement = new Audio ("./Audios/Audio9.mp3");
 
-    // einzelne Drumpads 
+    // Funktion
 
-    document.getElementById("pad1").addEventListener("click", function (): void {
-        playSample(0);
-        recordBeat(0);
-    });
+    function playSample(pad: HTMLAudioElement): void {
+        pad.play();
+        recordBeat(pad);
+    }
 
-    document.getElementById("pad2").addEventListener("click", function (): void {
-        playSample(1);
-        recordBeat(1);
-    });
+    document.getElementById("pad1").addEventListener("click", playSample.bind(null, pad1));
+    document.getElementById("pad2").addEventListener("click", playSample.bind(null, pad2));
+    document.getElementById("pad3").addEventListener("click", playSample.bind(null, pad3));
+    document.getElementById("pad4").addEventListener("click", playSample.bind(null, pad4));
+    document.getElementById("pad5").addEventListener("click", playSample.bind(null, pad5));
+    document.getElementById("pad6").addEventListener("click", playSample.bind(null, pad6));
+    document.getElementById("pad7").addEventListener("click", playSample.bind(null, pad7));
+    document.getElementById("pad8").addEventListener("click", playSample.bind(null, pad8));
+    document.getElementById("pad9").addEventListener("click", playSample.bind(null, pad9));
 
-    document.getElementById("pad3").addEventListener("click", function (): void {
-        playSample(2);
-        recordBeat(2);
-    });
+//Buttons: Mülleimer, Play, Pause, Micro, info
 
-    document.getElementById("pad4").addEventListener("click", function (): void {
-        playSample(3);
-        recordBeat(3);
-    });
+var playButton: HTMLElement = document.getElementById("play");
+var pauseButton: HTMLElement = document.getElementById("pause");
+var microButton: HTMLElement = document.getElementById("micro");
+var muellButton: HTMLElement = document.getElementById("muell");
+var infoButton: HTMLElement = document.getElementById("info");
+var infoText: HTMLElement = document.getElementById("bild");
 
-    document.getElementById("pad5").addEventListener("click", function (): void {
-        playSample(4);
-        recordBeat(4);
-    });
+// Der Anfangs-Beat
 
-    document.getElementById("pad6").addEventListener("click", function (): void {
-        playSample(5);
-        recordBeat(5);
-    });
+var anfangsBeat: HTMLAudioElement [] = [];
 
-    document.getElementById("pad7").addEventListener("click", function (): void {
-        playSample(6);
-        recordBeat(6);
-    });
+//Überprüfung von false/true
 
-    document.getElementById("pad8").addEventListener("click", function (): void {
-        playSample(7);
-        recordBeat(7);
-    });
+var is: boolean;
 
-    document.getElementById("pad9").addEventListener("click", function (): void {
-        playSample(8);
-        recordBeat(8);
-    });
+//Löschfunktion für den Mülleimer 
 
-// Funktion zum Abspielen lassen 
+function deletBeat (): void {
+    anfangsBeat.splice(0, anfangsBeat.length);
+    console.log(anfangsBeat.length);
+}
 
-    function playSample(i: number): void {
-        sample[i].play();
-        sample[i].recordBeat[];
-    };
+//Veränderung des Micro-Buttons wenn er "aktiv" ist
 
-//Play- und Pause-Button
+window.addEventListener ("load", function (): void {
+microButton.addEventListener("click", function (): void {
 
-const playButton: HTMLElement = document.getElementById("play");
-const pauseButton: HTMLElement = document.getElementById("pause");
-let array: number [] = [];
-let padAufnahme: boolean;
+    if (microButton.classList.contains("inactiv")) {
+        this.classList.remove("inactiv");
+        this.classList.add("activ")
+        is = true;
+    }
+    else {
+        microButton.classList.contains("activ");
+        this.classList.remove("activ");
+        this.classList.add("inactiv")
+        is = false;
+    }
 
-//Play- und Pause-Button verschwinden lassen
+    console.log(is);
+    console.log(anfangsBeat.length);
+});
+});
+
+// Löschen beim "click" auf den Mülleimer-Button
+
+muellButton.addEventListener("click", function (): void {
+    deletBeat();
+    console.log(anfangsBeat.length)
+});
+
+//Aufnahmefunktion
+
+function recordBeat (pad: HTMLAudioElement): void {
+    console.log(is);
+    if (is == true) {
+        anfangsBeat.push(pad);
+        console.log(anfangsBeat.length);
+}};
+
+//Wiedergabe/Play- und Paus-Button
+
+var interval: number;
+var beat: HTMLAudioElement [] = [];
+var index: number = 0;
+
+function playLoop (): void {
+    interval = setInterval (function () {
+
+        if(index < beat.length) {
+            playSample(beat [index]); index++;
+        } else {index = 0;}
+    }, 300);
+};
+
+playButton.addEventListener("click", function (): void {
+    interval = setInterval( function (): void {
+        if (index < anfangsBeat.length) {
+        playSample(anfangsBeat[index]);
+        index++;
+        }
+        else {
+            index = 0
+        }
+    }, 500);
+});
 
 playButton.addEventListener("click", function (): void {
     this.classList.add("is-hidden");
     pauseButton.classList.remove("is-hidden");
-    padAufnahme =true;
-
-})
+});
 
 pauseButton.addEventListener("click", function (): void {
     this.classList.add("is-hidden");
     playButton.classList.remove("is-hidden");
-})
-
-
-//Microfon-Button + Mülleimer-Button
-
-const microButton: HTMLElement = document.getElementById("micro");
-const muellButton: HTMLElement = document.getElementById("muell");
-
-microButton.addEventListener("click", function(): void {
-    if (microButton.classList.contains("activ")) {
-        this.classList.remove("activ");
-        padAufnahme = false;
-    } 
-    else {
-        microButton.classList.add("activ");
-        padAufnahme = true;
-    }
 });
 
-//Funktion zum aufnehmen
+pauseButton.addEventListener("click", function (): void {
+    clearInterval(interval);
+});
 
-function recordBeat (index: number): void {
-    if (padAufnahme == true) {
-        array.push(index);
-}}
+//Info-Button
 
-//Play und Pause
+infoButton.addEventListener("click", function (): void {
+    this.classList.add("activ");
+    infoText.classList.remove("is-hidden");
+});
 
-let interval: number;
-let o: number= 0;
+//Bedienen der Pads mit der Tastatur
 
-playButton.addEventListener("click", function(): void {
-        if (padAufnahme == true) {
-        interval = setInterval (function():void {
-       
-            if ((o < array.length)) {
-                playSample(array[o]);
-                o++;
-            }
-            else {
-                o = 0;
-            }
-        }, 300);
-    }       else {
-                clearInterval(interval);
-    }});
+document.addEventListener("keydown", function(event: KeyboardEvent): void {
+    switch (event.key) {
+        case "z":
+            playSample(pad1);
+            recordBeat(pad1);
+            break;
+        case "u":
+            playSample(pad2);
+            recordBeat(pad2);
+            break;
+        case "i":
+            playSample(pad3);
+            recordBeat(pad3);
+            break;
+        case "h":
+            playSample(pad4);
+            recordBeat(pad4);
+            break;
+        case "j":
+            playSample(pad5);
+            recordBeat(pad5);
+            break;
+        case "k":
+            playSample(pad6);
+            recordBeat(pad6);
+            break;
+        case "b":
+            playSample(pad7);
+            recordBeat(pad7);
+            break;
+        case "n":
+            playSample(pad8);
+            recordBeat(pad8);
+            break;
+        case "m":
+            playSample(pad9);
+            recordBeat(pad9);
+            break;
+    }
+});
